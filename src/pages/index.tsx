9c3bf9c05b87a7072062b9d0ba5e52e260658ab2
Hasan Gökçe <kodyap@gmail.com>
2024-08-28 00:11:15 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Tutorial - 4mins ⏱️
+            Tutorial - 3mins ⏱️
           </Link>
         </div>
       </div>
